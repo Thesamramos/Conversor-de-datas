5545a52d4f8ee9f4109a1a2354f43ad22e01f4b5
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2024-06-04 15:10:31 -0400

    opção de escolha da data atual e loop while

--- conversor_de_datas.py
+++ conversor_de_datas.py
@@ -2,33 +2,48 @@ import arrow
 
 print("====== CONVERSOR DE DATAS ======")
 
-day = int(input("Informe o dia (2 digitos): "))
-month = int(input("Informe o mês (2 digitos): "))
-year = int(input("informe o ano (4 digitos): "))
+print('[1] Usar uma data diferente\
+      [2] Usar data atual')
 
-date = arrow.get(f"{year}-{month}-{day}")
+date_type =  int(input("Informe o número da sua escolha: "))
 
-print("\n[1] Formado Internacional\
-      [2] Formato dos EUA\
-      [3] Formato da Europa\
-      [4] Formato da Ásia\
-      [5] Formato Verbal")
+if date_type == 1:
+    day = int(input("\nInforme o dia (2 digitos): "))
+    month = int(input("Informe o mês (2 digitos): "))
+    year = int(input("informe o ano (4 digitos): "))
 
-choice = int(input("Escolha um formato de data (digite o número): "))
+    date = arrow.get(f"{year}-{month}-{day}")
+else:
+    date = arrow.utcnow()
 
-def return_date_format(choice):
-    match choice:
-        case 1:
-            return "\nData formatada para o padrão internacional: " + date.format("YYYY-MM-DD")
-        case 2:
-            return "\nData formatada para o padrão dos EUA: " + date.format("MM/DD/YYYY")
-        case 3:
-            return "\nData formatada para o padrão europeu: " + date.format("DD/MM/YYYY")
-        case 4:
-            return "\nData formatada para o padrão asiatico: " + date.format("YYYY/MM/DD")
-        case 5:
-            return "\nData formatada para o padrão verbal: " + date.format("DD - MMMM - YYYY", locale="pt-br").replace("-", "de")
-        case _:
-            return "\nOpção Inválida"
+while True:
 
-print(return_date_format(choice))
+    print("\n[1] Formato Internacional\
+        [2] Formato dos EUA\
+        [3] Formato da Europa\
+        [4] Formato da Ásia\
+        [5] Formato Verbal\
+        [6] Sair")
+
+    choice_date_format = int(input("Escolha um formato de data (digite o número): "))
+
+    if choice_date_format == 6:
+        print("\nPrograma encerrado.")
+        break
+
+    def return_date_format(choice_date_format):
+        match choice_date_format:
+            case 1:
+                return "\nData formatada para o padrão internacional: " + date.format("YYYY-MM-DD")
+            case 2:
+                return "\nData formatada para o padrão dos EUA: " + date.format("MM/DD/YYYY")
+            case 3:
+                return "\nData formatada para o padrão europeu: " + date.format("DD/MM/YYYY")
+            case 4:
+                return "\nData formatada para o padrão asiatico: " + date.format("YYYY/MM/DD")
+            case 5:
+                return "\nData formatada para o padrão verbal: " + date.format("DD - MMMM - YYYY", locale="pt-br").replace("-", "de")
+            case _:
+                return "\nOpção Inválida"
+
+    print(return_date_format(choice_date_format))
